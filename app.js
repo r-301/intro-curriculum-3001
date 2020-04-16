ef97e56636c19adf0d84d044ed2d52250c0043f1
r-301 <>
2020-04-16 16:31:37 +0900

    会場関数の実装

--- app.js
+++ app.js
@@ -8,6 +8,9 @@
 function factorial(n) {
     let result = 1;
     // TODO このコメントを消して正しく実装してください。
+    for (let i = 0;i < n;i++){
+    result = (n - i) * result;
+    }
     return result;
 }
 const assert = require('assert');
